8f467d045f3886b01c31bfdabfe1848a6dbf8942
Alexey Ershov <alexa3mail@gmail.com>
2025-05-01 10:44:08 +0300

    alacritty -> ghostty

--- pkgs/setup-workflow-atlas.nix
+++ pkgs/setup-workflow-atlas.nix
@@ -1,6 +1,6 @@
 {writeShellScriptBin, ...}:
 writeShellScriptBin "setup-workflow-atlas" ''
-  hyprctl dispatch exec "[workspace 1 silent] alacritty"
+  hyprctl dispatch exec "[workspace 1 silent] ghostty"
   hyprctl dispatch exec "[workspace 2 silent] zen"
   hyprctl dispatch exec "[workspace 3 silent] vesktop"
   hyprctl dispatch exec "[workspace 3 silent] telegram-desktop"
--- pkgs/setup-workflow-aurora.nix
+++ pkgs/setup-workflow-aurora.nix
@@ -1,6 +1,6 @@
 {writeShellScriptBin, ...}:
 writeShellScriptBin "setup-workflow-aurora" ''
-  hyprctl dispatch exec "[workspace 1 silent] alacritty"
+  hyprctl dispatch exec "[workspace 1 silent] ghostty"
   hyprctl dispatch exec "[workspace 2 silent] zen"
   hyprctl dispatch exec "[workspace 10 silent] vesktop"
   hyprctl dispatch exec "[workspace 10 silent] telegram-desktop"

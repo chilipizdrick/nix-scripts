c1e9aaf29e8e04fcb26a2573a3c48d605495c8df
Alexey Ershov <alexa3mail@gmail.com>
2025-08-03 12:03:37 +0300

    take zapret service into account when toggling tailscale

--- pkgs/toggle-tailscale.nix
+++ pkgs/toggle-tailscale.nix
@@ -5,8 +5,10 @@
 }:
 writeShellScriptBin "toggle-tailscale" ''
   if [[ "$(${tailscale}/bin/tailscale status)" == "Tailscale is stopped." ]]; then
+      systemctl stop zapret.service
       ${tailscale}/bin/tailscale up
   else
       ${tailscale}/bin/tailscale down
+      systemctl start zapret.service
   fi
 ''

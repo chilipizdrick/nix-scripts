f3cd38a7b62454dd0468df9f6289ef480bbcd5f9
Alexey Ershov <alexa3mail@gmail.com>
2025-08-11 22:32:49 +0300

    remove zapret toggling from tailscale scripts

--- pkgs/toggle-tailscale.nix
+++ pkgs/toggle-tailscale.nix
@@ -5,10 +5,8 @@
 }:
 writeShellScriptBin "toggle-tailscale" ''
   if [[ "$(${tailscale}/bin/tailscale status)" == "Tailscale is stopped." ]]; then
-      systemctl stop zapret.service
       ${tailscale}/bin/tailscale up
   else
       ${tailscale}/bin/tailscale down
-      systemctl start zapret.service
   fi
 ''

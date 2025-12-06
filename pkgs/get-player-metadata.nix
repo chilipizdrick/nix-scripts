11ee382a1b539f44ff9ac5d1f0962d4e4acdb59d
Alexey Ershov <alexa3mail@gmail.com>
2025-12-06 23:13:24 +0300

    change paused symbol in player metadata script

--- pkgs/get-player-metadata.nix
+++ pkgs/get-player-metadata.nix
@@ -8,7 +8,7 @@ writeShellScriptBin "get-player-metadata" ''
       printf ""
       exit 0
   elif [[ "$(${playerctl}/bin/playerctl metadata | head -1 | awk '{print $1}')" != "firefox" ]]; then
-      printf "<b>$([ "$(${playerctl}/bin/playerctl status)" != "Paused" ] && [ "$(${playerctl}/bin/playerctl metadata | head -1 | awk '{print $1}')" = "spotify" ] && echo ' ')$([ "$(${playerctl}/bin/playerctl status)" = "Paused" ] && echo '|| ')$(${playerctl}/bin/playerctl metadata title | sed 's/\(.\{30\}\).*/\1.../')</b>\n$(${playerctl}/bin/playerctl metadata artist | sed 's/\(.\{30\}\).*/\1.../')"
+      printf "<b>$([ "$(${playerctl}/bin/playerctl status)" != "Paused" ] && [ "$(${playerctl}/bin/playerctl metadata | head -1 | awk '{print $1}')" = "spotify" ] && echo ' ')$([ "$(${playerctl}/bin/playerctl status)" = "Paused" ] && echo ' ')$(${playerctl}/bin/playerctl metadata title | sed 's/\(.\{30\}\).*/\1.../')</b>\n$(${playerctl}/bin/playerctl metadata artist | sed 's/\(.\{30\}\).*/\1.../')"
       exit 0
   else
       printf ""
